1f649d98a1ff8c5c8204bb5d17e1f33085724c25
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-12-06 20:52:02 -0500

    incorporate Index.ImgLookup

--- src/Index/ImgLookup.hs
+++ src/Index/ImgLookup.hs
@@ -1,45 +1,43 @@
-{-# OPTIONS_CYMAKE -F --pgmF=currypp --optF=defaultrules #-}
-
 module Index.ImgLookup where
 
-import Maybe
-import qualified Map as M
+import           Data.Maybe (catMaybes, isNothing, fromJust)
+import qualified Data.Map as M
 
 import Rslt
 import Util (hasANothing)
 
 
 exprVariety :: Expr -> (Expr', Arity)
-exprVariety   (Word      _)   = (Word'      , 0)
-exprVariety e@(Tplt  _)   = (Tplt'  , arity e)
-exprVariety e@(Rel       _ _) = (Rel'       , arity e)
-exprVariety e@(Par _ _) = (Par' , arity e)
+exprVariety   (Word  _) = (Word' , 0)
+exprVariety e@(Tplt  _) = (Tplt' , arity e)
+exprVariety e@(Rel _ _) = (Rel'  , arity e)
+exprVariety e@(Par _ _) = (Par'  , arity e)
 
-imgDb :: Files -> FM Expr Addr
-imgDb = listToFM (<) . catMaybes . map f . fmToList where
+imgDb :: Files -> M.Map Expr Addr
+imgDb = M.fromList . catMaybes . map f . M.toList where
   f (addr, expr) = case exprImgKey expr of
     Nothing -> Nothing
     _       -> Just (expr, addr)
 
   exprImgKey :: Expr -> Maybe Expr
   exprImgKey e = case e of Par _ _ -> Nothing
-                           _             -> Just e
+                           _       -> Just e
 
 imgLookup :: Files -> ImgOfExpr -> Maybe Addr
 imgLookup files img = let idb = imgDb files in case img of
 
-  ImgOfExpr    e -> lookupFM idb e
-  ImgOfAddr a -> maybe Nothing (const $ Just a) $ lookupFM files a
+  ImgOfExpr e -> M.lookup e idb
+  ImgOfAddr a -> maybe Nothing (const $ Just a) $ M.lookup a files
 
   ImgOfTplt is ->
     let mas = map (imgLookup files) is
     in case hasANothing mas of
-         True -> Nothing
-         False -> lookupFM idb $ Tplt $ catMaybes mas
+         True  -> Nothing
+         False -> M.lookup (Tplt $ catMaybes mas) idb
 
   ImgOfRel is i ->
     let mas = map (imgLookup files) is
         ma = imgLookup files i
     in case hasANothing mas || isNothing ma of
-         True -> Nothing
-         False -> lookupFM idb $ Rel (catMaybes mas) $ fromJust ma
+         True  -> Nothing
+         False -> M.lookup (Rel (catMaybes mas) $ fromJust ma) idb
