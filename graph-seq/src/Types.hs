53daa68115441cbf20b3884dd91ace8ca0057941
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-01-05 14:39:53 -0500

    pseudocode like mad

--- graph-seq/src/Types.hs
+++ graph-seq/src/Types.hs
@@ -4,26 +4,40 @@ import Data.Map
 import Data.Set
 
 
-data Find = Find
-data Cond = Cond
+data Graph = Graph { children :: Map Int Int   -- ^ keys here are parents
+                   , parents  :: Map Int Int } -- ^ keys here are children
+  deriving (Show, Eq, Ord)
 
-data Graph = Graph { children :: Map Int Int   -- ^ keys are parents
-                   , parents  :: Map Int Int } -- ^ keys are children
+newtype Var = Var String deriving (Show, Eq, Ord)
+data Elt = Ground Int | VarElt Var -- TODO ? Use these in QFind, QCond.
+  deriving (Show, Eq, Ord)
+data VarFunc = VarFunc {
+  varName :: Var
+  , varDets :: Set Var }
+  deriving (Show, Eq, Ord)
 
-newtype Var = Var String
+data Program = Program { programGraph   :: Graph
+                       , programQueries :: [ (Var, Query) ]
+                       , programOutput  :: [ VarFunc ] }
 
-data Program = Program { programGraph  :: Graph
-                       , programOutput :: [Var]
-                       , program       :: [(Var,Query)] }
-
+type Find = Graph -> Subst -> Set Int
+type Cond = Graph -> Subst ->     Int -> Bool
 data Query = QFind Find
            | QCond Cond
-           | QAnd [Query]
-           | QOr [Query]
-           | ForAll [Var] Query
-           | ForSome [Var] Query
+           | QNot  Cond
+           | QIntersect [Query]
+           | QUnion     [Query]
+           | ForAll  [VarFunc] Query
+           | ForSome [VarFunc] Query -- TODO ? process in terms of ForAll
 
-type Binding = (Var, Int)
-
--- | Note that the empty set of bindings is a valid value.
-type VarResult = (Var, Map Int (Set Binding))
+type Subst = Map Var Int
+-- | `VarResult` is used for partial as well as complete results.
+--
+-- `VarResult` example: suppose that in the VarResult R, Y could be
+-- bound to 1 under either of the following conditions:
+--     when X is bound to 2
+--     when X is bound to 3 and Z is bound to 4
+-- In that case, M.lookup 1 (M.lookup Y R) =
+--     S.fromList [ S.singleton  (X,2)
+--                , S.fromList [ (X,3), (Z,4) ] ]
+type Result = Map Var (Map Int (Set Subst))
