472811869ae168637979c5e92ab4ee269ac4e026
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-01-05 21:46:48 -0500

    untested   +findable :: Query -> Bool   +validExistentials :: Query -> Bool discover Queries do not belong in Sets.   which is fine, b/c I don't need random access to them.

--- graph-seq/src/Query.hs
+++ graph-seq/src/Query.hs
@@ -18,4 +18,30 @@ couldBind (QUnion     qs) = S.unions   $ S.map couldBind qs
 couldBind (QIntersect qs) = S.unions   $ S.map couldBind qs
 couldBind (ForSome vfs q) = S.union vs $       couldBind q
   where vs = S.map varFuncName vfs
-couldBind (ForAll  _  q) =                     couldBind q
+couldBind (ForAll  _   q) =                    couldBind q
+
+-- | Every `QIntersect` must include something `findable`, and
+-- every `QUnion` must consist entirely of `findable` things.
+findable :: Query -> Bool
+findable (QFind _)       = True
+findable (QCond _)       = False
+findable (QIntersect qs) = S.foldl (||) False $ S.map findable qs
+findable (QUnion     qs) = S.foldl (&&) True  $ S.map findable qs
+findable (ForSome vfs q) = findable q
+findable (ForAll  _   q) = findable q
+
+validExistentials :: Query -> Bool
+validExistentials (ForSome vfs q)
+  = S.disjoint (S.map varFuncName vfs) (couldBind q)
+validExistentials (QIntersect qs) = snd $ S.foldl f (S.empty, True) qs
+  where f :: (Set Var, Bool) -> Query -> (Set Var, Bool)
+        f (_, False) _ = (S.empty, False)
+        f (vs, True) q = if S.disjoint vs $ couldBind q
+                         then (S.union vs $ couldBind q, True)
+                         else (S.empty, False)
+  -- If that doesn't work, here's a (less efficient) definition:
+  -- validExistentials q@(QIntersect qs) = size == sizes where
+  -- -- Inefficient, because it won't short-circuit.
+  -- size  = S.size $ couldBind q
+  -- sizes = S.foldl (+) 0 $ S.map (S.size . couldBind) qs
+validExistentials _ = True
--- graph-seq/src/Test.hs
+++ graph-seq/src/Test.hs
@@ -7,13 +7,33 @@ import qualified Data.Set       as S
 import Test.HUnit
 
 import Graph
+import Query
 import Types
 
 
 tests = runTestTT $ TestList
-  [ TestLabel "graphTest" graphTest ]
+  [ TestLabel "testInvertMapToSet" testInvertMapToSet
+--  , TestLabel "testFindable" testFindable
+  ]
 
-graphTest = TestCase $ do
+--testFindable = TestCase $ do
+--  let qf = QFind $ \_ _    -> S.empty
+--      qc = QCond $ \_ _  _ -> False
+--      label = "testFindable"
+--  assertBool label $ (findable $ QIntersect (S.fromList [qf, qc])) == True
+--  assertBool label $ (findable $ QIntersect (S.fromList [qc])    ) == False
+--  assertBool label $ (findable $ QIntersect (S.fromList [])      ) == False
+--  assertBool label $ (findable $ QUnion     (S.fromList [qf, qc])) == False
+--  assertBool label $ (findable $ QUnion     (S.fromList [qc])    ) == True
+--  assertBool label $ (findable $ QUnion     (S.fromList [])      ) == True
+
+--  , TestLabel "testValid" testValid
+--testValid = TestCase $ do
+--  let qx = ForAll (S.singleton $ Var "x") $ 
+--  assertBool "testValid" $ True ==
+--    (valid $ 
+
+testInvertMapToSet = TestCase $ do
   let g = M.fromList [ (1, S.fromList [2,3] )
                      , (2, S.fromList [1,3] ) ]
       h = M.fromList [ (1, S.fromList [2  ] )
