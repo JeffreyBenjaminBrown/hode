29314d105d60f2c484376bb2e5a0a5caaa0768f5
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-01-10 16:21:00 -0500

    subdivide Query.Classify from Query

--- graph-seq/src/Query.hs
+++ graph-seq/src/Query.hs
@@ -11,58 +11,8 @@ import qualified Data.Set       as S
 
 import Subst
 import Types
+import Query.Classify
 
-
--- | = Testing that a Query is valid.
-
--- | A `Query`, if it is `ForSome v _` or `ForAll v _`, introduces `v`.
--- And every `Query` introduces whatever its subqueries introduces.
-introduces :: Query -> Set Var
-introduces (QOr  qs)      = S.unions    $ map introduces qs
-introduces (QAnd qs)      = S.unions    $ map introduces qs
-introduces (ForSome vf q) = S.insert vf $     introduces q
-introduces (ForAll  vf q) = S.insert vf $     introduces q
-introduces _ = S.empty
-
--- | The only way a `Var` could be bound is if it was introduced by
--- a `ForSome`.
-couldBind :: Query -> Set Var
-couldBind (QOr  qs)      = S.unions    $ map couldBind qs
-couldBind (QAnd qs)      = S.unions    $ map couldBind qs
-couldBind (ForSome vf q) = S.insert vf $     couldBind q
-couldBind (ForAll  _  q) =                   couldBind q
-couldBind _              = S.empty
-
--- | Every `QAnd` must include something `findable`, and
--- every `QOr` must be nonempty and consist entirely of `findable` queries.
-findable, testable :: Query -> Bool
-findable (QFind _)          = True
-findable (QTest _)          = False
-findable (QAnd qs)          = or  $ map findable qs
-findable (QOr     [])       = False
-findable (QOr     qs@(_:_)) = and $ map findable qs
-findable (ForSome _ q)      = findable q
-findable (ForAll  _ q)      = findable q
-
-testable = not . findable
-
--- | A `Query` is only valid if no quantifier masks an earlier one,
--- and if no `
-disjointExistentials :: Query -> Bool
-disjointExistentials (ForSome vf q)
-  = not $ S.member vf $ couldBind q
-disjointExistentials (ForAll vf q)
-  = not $ S.member vf $ couldBind q
-disjointExistentials (QAnd qs) = snd $ foldr f (S.empty, True) qs
-  where f :: Query -> (Set Var, Bool) -> (Set Var, Bool)
-        f _ (_, False) = (S.empty, False) -- short circuit (hence foldr)
-        f q (vs, True) = if S.disjoint vs $ couldBind q
-                         then (S.union vs $ couldBind q, True)
-                         else (S.empty, False)
-disjointExistentials _ = True
-
-
--- | Running queries.
 
 runFind :: Data -> Subst -> Find -> CondElts
 runFind d s (Find find deps) =
--- graph-seq/src/Test/TQuery.hs
+++ graph-seq/src/Test/TQuery.hs
@@ -9,12 +9,13 @@ import Test.HUnit
 
 import Graph
 import Query
+import Query.Classify
 import Types
 
 
 testModuleQuery = TestList [
     TestLabel "testFindable" testFindable
-  , TestLabel "testDisjointExistentials" testDisjointExistentials
+  , TestLabel "testOkExistentials" testOkExistentials
   , TestLabel "testQFind" testQFind
   , TestLabel "testForSome" testForSome
   , TestLabel "testForAll" testForAll
@@ -89,17 +90,17 @@ testQFind = TestCase $ do
     == M.fromList [ (12, S.singleton $ M.singleton y 2)
                   , (22, S.singleton $ M.singleton y 2) ]
 
-testDisjointExistentials = TestCase $ do
+testOkExistentials = TestCase $ do
   let qf  = QFind $ Find (\_ _ -> S.empty) S.empty
       x   = Var "x" $ S.empty
       y   = Var "y" $ S.empty
       qx  = ForSome x qf
       qy  = ForSome y qf
       qxy = QAnd [qx,qy]
-  assertBool "1" $ disjointExistentials (ForSome x qx)  == False
-  assertBool "2" $ disjointExistentials (ForSome y qx)  == True
-  assertBool "3" $ disjointExistentials qxy             == True
-  assertBool "4" $ disjointExistentials (QAnd [qx,qxy]) == False
+  assertBool "1" $ okExistentials (ForSome x qx)  == False
+  assertBool "2" $ okExistentials (ForSome y qx)  == True
+  assertBool "3" $ okExistentials qxy             == True
+  assertBool "4" $ okExistentials (QAnd [qx,qxy]) == False
 
 testFindable = TestCase $ do
   let qf = QFind $ Find (\_ _    -> S.empty) S.empty
