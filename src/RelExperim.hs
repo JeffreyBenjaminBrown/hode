2a26e2e200f78ad0cf64ba04032a446ee068187b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-01-20 12:40:27 -0500

    clarify names

--- src/RelExperim.hs
+++ src/RelExperim.hs
@@ -20,14 +20,14 @@ type Rel = R.Relation
 type CondElts' e = Rel e (Subst e)
 type Possible' e = Map Var (CondElts' e)
 
-data TSource = TSource  { plans   :: [TSourcePlan]
-                        , inputs  :: [Var]
-                        , outputs :: [Var] }
-data TSourcePlan = TSourcePlan {
-    tSource     :: Var
-  , tName       :: Var
-  , inputNames  :: [Var]
-  , outputNames :: [Var] }
+data TSource = TSource { plans   :: [TVarPlan]
+                       , haveInputs  :: [Var]
+                       , haveOutputs :: [Var] }
+
+data TVarPlan = TVarPlan { tSource             :: Var
+                         , tName               :: Var
+                         , namesItsInputs      :: [Var]
+                         , isNamedByItsOutputs :: [Var] }
 
 -- | If s binds i to i0 and o to o0, calling
 -- `varPossibilities p s (TSource v (S.singleton i) (S.singleton o))`,
@@ -38,40 +38,41 @@ varPossibilities :: forall e. (Ord e, Show e)
                  => Possible e -> Subst e -> TSource
                  -> Either String (Possible e)
 varPossibilities    p           s            t = let
-  (pls, ins) = (plans t, inputs t)
+  (pls, ins) = (plans t, haveInputs t)
   se, lefts :: [Either String (CondElts e)]
   se = map (f . tSource) pls where
     f :: Var -> Either String (CondElts e)
     f v = maybe (Left $ keyErr "varPossibilities" v p) Right
           $ M.lookup v p
+
   lefts = filter isLeft se
   in case null lefts of
   False -> Left $ "varPossibilities: error in callee:\n"
            ++ (foldr (++) "" $ map (fromLeft "") lefts)
-
   True -> let
     ces :: [CondElts e]
     ces = map (fromRight $ error "impossible") se
+
     ces_either, lefts :: [Either String (CondElts e)]
     ces_either = map f $ zip pls ces
-      where f :: (TSourcePlan, CondElts e) -> Either String (CondElts e)
+      where f :: (TVarPlan, CondElts e) -> Either String (CondElts e)
             f (pl, ce) = restrictToMatchIns t pl s ce
     lefts = filter isLeft ces_either
     in case null lefts of
     False -> Left $ "varPossibilities: error in callee:\n"
              ++ (foldr (++) "" $ map (fromLeft "") lefts)
-
     True -> let
       cesRestricted = map (fromRight $ error "impossible") ces_either
+
       in Right $ M.fromList $ zip (map tName pls) cesRestricted
     -- TODO : match outputs also
 
 restrictToMatchIns :: forall e. Eq e =>
-  TSource -> TSourcePlan -> Subst e
+  TSource -> TVarPlan -> Subst e
   -> CondElts e -> Either String (CondElts e)
 
 restrictToMatchIns t pl s ce = let
-  ins = inputs t
+  ins = haveInputs t
   sIns = M.restrictKeys s $ S.fromList ins :: Subst e
   s_either = M.mapKeys (renameIn t pl) sIns :: Map (Either String Var) e
   lefts = S.filter isLeft $ M.keysSet s_either :: Set (Either String Var)
@@ -85,9 +86,9 @@ restrictToMatchIns t pl s ce = let
     supermaps = M.map ( S.filter $ M.isSubmapOf sRenamed) ce
     in Right $ M.filter (not . S.null) $ supermaps
 
-renameIn :: TSource -> TSourcePlan -> Var -> Either String Var
-renameIn t pl k = let ins = inputs t
-                      newNames = inputNames pl
+renameIn :: TSource -> TVarPlan -> Var -> Either String Var
+renameIn t pl k = let ins = haveInputs t
+                      newNames = namesItsInputs pl
                       renamer = M.fromList $ zip ins newNames
   in maybe  (Left $ keyErr "renameInput" k renamer) Right
      $ M.lookup k renamer
--- src/Test/TRel.hs
+++ src/Test/TRel.hs
@@ -20,11 +20,28 @@ test_module_Rel = TestList [
   , TestLabel "test_restrictToMatchIns" test_restrictToMatchIns
   ]
 
+thinking = let (a, b, c, x, y, z)  = ("a", "b", "c", "x", "y", "z")
+               (a1,b1,c1,x1,y1,z1) = ("a1","b1","c1","x1","y1","z1")
+               (a2,b2,c2,x2,y2,z2) = ("a2","b2","c2","x2","y2","z2")
+  in varPossibilities -- Possible e -> Subst e -> TSource
+  ( M.fromList [ (x, ( M.fromList
+                       [ ( 21, ( S.singleton
+                                 $ M.fromList [(a,1), (b,11)] ) ) ] ) )
+               , (y, ( M.fromList
+                       [ ( 31, ( S.singleton
+                                 $ M.fromList [(a,1), (b,11)] ) ) ] ) ) ] )
+  ( M.fromList [(x1,21),(y1,31)] :: Subst Int)
+  ( TSource
+   [ ( TVarPlan a a1 [x1,y1] [] ) -- source, name, inputNames, outputNames
+   , ( TVarPlan b b1 [x2,y2] [] ) ]
+   []                                -- inputs
+   [x1, y1] )                        -- outputs
+
 test_restrictToMatchIns = TestCase $ do
   let meh = error "whatever"
   assertBool "1" $ restrictToMatchIns
-    (TSource [] ["a1","b1"] [])
-    (TSourcePlan "what" "ever" ["a","b"] [])
+    (TSource [] ["a1","b1"] [])   -- most args don't matter here
+    (TVarPlan "" "" ["a","b"] []) -- most args don't matter here
     (M.fromList [("a1",1),("b1",1)] :: Subst Int)
     (M.fromList [ ( 1, S.fromList [ M.fromList [("a",1),("b",1),("c",1)]
                                   , M.fromList [("a",2),("b",2)] ] )
@@ -37,5 +54,5 @@ test_renameIn = TestCase $ do
   let meh = error "whatev"
   assertBool "1" $ renameIn
     (TSource meh ["a1","b1"] meh)
-    (TSourcePlan meh meh ["a","b"] meh)
+    (TVarPlan meh meh ["a","b"] meh)
     "a1" == Right "a"
