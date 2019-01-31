219428e64654374a32d28db0c34c85e61ecda34f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-01-31 11:42:28 -0500

    untested   +readRslt :: FilePath -> IO (Rslt)

--- src/Data/Rslt/Files.hs
+++ src/Data/Rslt/Files.hs
@@ -9,13 +9,27 @@ import           Data.Map (Map)
 import qualified Data.Map       as M
 import           Data.Set (Set)
 import qualified Data.Set       as S
+import           System.Directory (listDirectory)
+import           System.FilePath.Posix (dropExtension, takeExtension)
 
 import Data.Rslt.RTypes
+import Data.Rslt.Index
 import Util
 
 
-write :: FilePath -> Rslt -> IO ()
-write p r = let
+readRslt :: FilePath -> IO (Rslt)
+readRslt p0 = do
+  files <- filter (\f -> takeExtension f == ".rslt")
+           <$> listDirectory p0
+  (es :: [(Addr, Expr)]) <- let
+      f p = do (e :: Expr) <- read <$> readFile p
+               let (a :: Addr) = read $ dropExtension p
+               return (a,e)
+      in mapM f files
+  return $ mkRslt $ M.fromList es
+
+writeRslt :: FilePath -> Rslt -> IO ()
+writeRslt p r = let
   writeExpr :: (Addr, Expr) -> IO ()
   writeExpr (a,e) =
     writeFile name $ show e
