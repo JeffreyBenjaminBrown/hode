1278562adaf9349f4d517fe21bcbfe5f5600c9a6
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-02-23 16:40:29 -0500

    IO, read and write: works!   +pFindStrings :: Rslt -> String -> Either String (Set String)   +pFindStringsIO :: Rslt -> String -> IO ()

--- src/Experim.hs
+++ src/Experim.hs
@@ -3,16 +3,18 @@
 module Experim where
 
 import           Data.Set (Set)
---import qualified Data.Set as S
+import qualified Data.Set as S
 import Text.Megaparsec
 
 import Hash.Convert
 import Hash.HLookup
 import Hash.HParse
-import Hash.HTypes
+--import Hash.HTypes
 import Qseq.QTypes
 import Rslt.Edit
+import Rslt.RLookup
 import Rslt.RTypes
+import Rslt.Show
 import Util.Misc
 
 
@@ -23,23 +25,29 @@ pInsert r s = prefixLeft "pInsert"
   >>= hExprToExpr r
   >>= exprToAddrInsert r
 
--- Right p = pPExpr "/hash _ #like _"
--- bad: does not recognize the empty outer joints on the template,
--- at least if they are Any (_).
-pPExpr :: String -> Either String PExpr
-pPExpr s = prefixLeft "pShow"
-  $ mapLeft show (parse pExpr "doh!" s)
-
--- Right h = pHExpr "/hash _ #like _"
-pHExpr :: String -> Either String HExpr
-pHExpr s = prefixLeft "pShow"
-  $ mapLeft show (parse pExpr "doh!" s)
-  >>= pExprToHExpr
-
--- pFind D.b2 "/hash _ #like _"
--- import qualified Test.Rslt.RData as D
-pFind :: Rslt -> String -> Either String (Set Addr)
-pFind r s = prefixLeft "pShow"
+pFindAddrs :: Rslt -> String -> Either String (Set Addr)
+pFindAddrs r s = prefixLeft "pFindAddrs"
   $ mapLeft show (parse pExpr "doh!" s)
   >>= pExprToHExpr
   >>= hExprToAddrs r (mempty :: Subst Addr)
+
+pFindStrings :: Rslt -> String -> Either String (Set String)
+pFindStrings r s = do
+  (as :: Set Addr) <- prefixLeft "pFindExprs"
+                      $ pFindAddrs r s
+  (es :: Set Expr) <- ifLefts_set "pFindExprs"
+                      $ S.map ( addrToExpr r ) as
+  (ss :: Set String) <- ifLefts_set "pFindExprs"
+                        $ S.map (eShow r) es
+  return ss
+
+pFindStringsIO :: Rslt -> String -> IO ()
+pFindStringsIO r s = do
+  let (e :: Either String (Set String)) =
+        pFindStrings r s
+  case e of
+    Left err -> putStrLn err
+    Right ss -> mapM_ putStrLn ss
+
+-- import qualified Test.Rslt.RData as D
+-- pFindStringsIO D.b2 "/hash _ #like _"
