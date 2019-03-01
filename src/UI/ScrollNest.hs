4df266d0158c1cdfbb7e1a8a55656c881652df48
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-02-28 23:10:20 -0500

    toward ScrollNest.hs

--- src/UI/ScrollNest.hs
+++ src/UI/ScrollNest.hs
@@ -1,12 +1,24 @@
--- | Based on the demos in the programs/ folder of Brick,
--- particularly `EditDemo.hs`.
+-- | Goal: Windows can contain windows, and subwindows can scroll
+-- within their parent window.
+-- To start there is only one window, an editor with a single number.
+-- The user can put a "contents" sub-window (itself not an editor)
+-- in that editor, or any later-created editor.
+-- A contents subwindow can contain more editors.
+-- The user can scroll to the previous, next, parent, or first
+-- (or better: most recently accessed) child of any editor.
+-- A line containing an ellipsis appears at the start or the end of
+-- any set of contiguous subwindows, to indicate that more are off-screen.
 
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module UI.ScrollNest where
 
 import           Control.Monad.IO.Class (liftIO)
+import           Data.Tree
+import qualified Data.Tree as T
 import           Lens.Micro
+import           Lens.Micro.TH
 
 import qualified Brick.Main as B
 import qualified Brick.Types as B
@@ -18,21 +30,29 @@ import qualified Brick.Focus as B
 import           Brick.Util (on)
 import qualified Graphics.Vty as B
 
+
 -- | A path from the top window to the given window
 -- The top window has the name [].
 type Name = [Int] 
 
-data FocusTree = FocusLeaf | FocusTree Name [FocusTree]
+data Window = Window Name
 
 data St = St {
-    _focusTree :: FocusTree
-    }
+    _windows :: Tree Window
+  , _focus :: Name
+  }
+
+makeLenses ''St
+
+
+-- | = functions
 
 main :: IO St
-main = B.defaultMain app initialState
+main = mainFrom $ St { _windows = T.Node (Window []) []
+                     , _focus = [] }
 
-initialState :: St
-initialState = St $ FocusTree [] []
+mainFrom :: St -> IO St
+mainFrom = B.defaultMain app
 
 app :: B.App St e Name
 app = B.App
@@ -50,11 +70,11 @@ appDraw = error "?"
 -- maybe a map, they keys of which come from some tree field of the `St`).
 appChooseCursor ::
   St -> [B.CursorLocation Name] -> Maybe (B.CursorLocation Name)
-appChooseCursor = error "?"
+appChooseCursor _ _ = Nothing
 
 appHandleEvent ::
   St -> B.BrickEvent Name e -> B.EventM Name (B.Next St)
-appHandleEvent = error "?"
+appHandleEvent st _ = B.continue st
 
 appAttrMap :: B.AttrMap
 appAttrMap = B.attrMap B.defAttr
