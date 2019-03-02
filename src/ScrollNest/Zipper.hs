091d1bb745c64b6d6b3bfc9784e35a225d1cbdf8
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-03-02 08:10:33 -0500

    ScrollNest: everything but moving cursor

--- src/ScrollNest/Zipper.hs
+++ src/ScrollNest/Zipper.hs
@@ -33,70 +33,81 @@ type Path = [Int]
 
 type Ed = B.Editor String Path
 
-data St = St { _windows :: TreePos Full Ed
+data Window = Window { _isFocused :: Bool
+                     , _windowEd :: Ed }
+makeLenses ''Window
+
+data St = St { _windows :: TreePos Full Window
              , _focus :: () } -- ^ will use later
 makeLenses ''St
 
 
 ---- | Brick
---
---main :: IO St
---main = mainFrom aState
---
---mainFrom :: St -> IO St
---mainFrom = B.defaultMain app
+
+main :: IO St
+main = mainFrom aState
+
+mainFrom :: St -> IO St
+mainFrom = B.defaultMain app
 
 aState :: St
 aState = let
-  pw :: Path -> Ed
-  pw p = B.editor p (Just 1) $ show p
+  (pw :: Path -> Window) = \p -> Window
+    { _isFocused = False
+    , _windowEd = B.editor p (Just 1) $ show p }
 
-  pt :: Path -> Tree Int -> Tree Ed
+  pt :: Path -> Tree Int -> Tree Window
   pt p (Node i ns) = Node  (pw $ i : p)
                      $ map (pt $ i : p) ns
 
-  in St { _focus = ()
-        , _windows = Z.fromTree $ pt [] $
-          Node 0 [ Node 0 []
-                 , Node 1 [ Node 0 []
-                          , Node 1 [] ] ] }
+  (wz :: TreePos Full Window) = Z.fromTree $ pt [] $
+    Node 0 [ Node 0 []
+           , Node 1 [ Node 0 []
+                    , Node 1 [] ] ]
 
---app :: B.App St e Path
---app = B.App
---  { B.appDraw         = appDraw
---  , B.appChooseCursor = appChooseCursor
---  , B.appHandleEvent  = appHandleEvent
---  , B.appStartEvent   = return
---  , B.appAttrMap      = const appAttrMap
---  }
---
---treeDraw :: St -> Tree Window -> B.Widget Path
---treeDraw st (Tree (Window p e) ws) =
---  B.renderEditor (str . unlines) (V.reverse p == st ^. focus) e
---  <=> padLeft (B.Pad 2) (vBox $ map (treeDraw st) $ V.toList ws)
---
---appDraw :: St -> [B.Widget Path]
---appDraw st = [treeDraw st $ st ^. windows]
---
----- | Ignore the list; this app needs cursor locations to be in a tree (or
----- maybe a map, keys of which are first drawn from a tree in the `St`).
---appChooseCursor ::
---  St -> [B.CursorLocation Path] -> Maybe (B.CursorLocation Path)
---appChooseCursor _ _ = Nothing
---
---appHandleEvent ::
---  St -> B.BrickEvent Path e -> B.EventM Path (B.Next St)
---appHandleEvent st (B.VtyEvent ev) = case ev of
---  B.EvKey B.KEsc []        -> B.halt st
---  B.EvKey (B.KChar 'i') [] -> B.continue $ st & focus .~ V.singleton 0
---  _ -> B.continue st
---appHandleEvent st _ = B.continue st
---
---appAttrMap :: B.AttrMap
---appAttrMap = B.attrMap B.defAttr
---    [ (B.editAttr,        B.white `on` B.black)
---    , (B.editFocusedAttr, B.black `on` B.yellow)
---    ]
---
+  in St { _focus = ()
+        , _windows = Z.modifyLabel (isFocused .~ True) wz }
+
+app :: B.App St e Path
+app = B.App
+  { B.appDraw         = appDraw
+  , B.appChooseCursor = appChooseCursor
+  , B.appHandleEvent  = appHandleEvent
+  , B.appStartEvent   = return
+  , B.appAttrMap      = const appAttrMap
+  }
+
+windowDraw :: Window -> B.Widget Path
+windowDraw w =
+  B.renderEditor (str . unlines) (w ^. isFocused) (w ^. windowEd)
+
+treeDraw :: St -> Tree Window -> B.Widget Path
+treeDraw st (Node w ws) =
+  windowDraw w
+  <=> padLeft (B.Pad 2) (vBox $ map (treeDraw st) ws)
+
+appDraw :: St -> [B.Widget Path]
+appDraw st = [treeDraw st $ Z.tree $ Z.root $ st ^. windows]
+
+-- | Ignore the list; this app needs cursor locations to be in a tree (or
+-- maybe a map, keys of which are first drawn from a tree in the `St`).
+appChooseCursor ::
+  St -> [B.CursorLocation Path] -> Maybe (B.CursorLocation Path)
+appChooseCursor _ _ = Nothing
+
+appHandleEvent ::
+  St -> B.BrickEvent Path e -> B.EventM Path (B.Next St)
+appHandleEvent st (B.VtyEvent ev) = case ev of
+  B.EvKey B.KEsc []        -> B.halt st
+  -- B.EvKey (B.KChar 'i') [] ->
+  _ -> B.continue st
+appHandleEvent st _ = B.continue st
+
+appAttrMap :: B.AttrMap
+appAttrMap = B.attrMap B.defAttr
+    [ (B.editAttr,        B.white `on` B.black)
+    , (B.editFocusedAttr, B.black `on` B.yellow)
+    ]
+
 --stFocusWindow :: St -> Maybe Window
 --stFocusWindow st = error "todo"
