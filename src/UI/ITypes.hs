05815ba17884cce7536b828a5c4d92f7895ca708
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-03-16 21:48:19 -0500

    UI.ITypes: write|change some Show instances   for which I had to change the order a lot

--- src/UI/ITypes.hs
+++ src/UI/ITypes.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -15,6 +16,8 @@ import qualified Brick.Focus as B
 
 import Hash.HTypes
 import Rslt.RTypes
+import Rslt.Show
+import Util.Misc (replaceNth)
 
 
 -- | = Tiny types
@@ -40,15 +43,61 @@ type Folder = String
 
 -- | = Meaty types
 
-data St = St {
-    _focusRing            :: B.FocusRing WindowName
-  , _viewTree             :: ViewTree
-  , _pathToFocus          :: Path
-  , _uiError              :: String
-  , _commands             :: B.Editor String WindowName
-  , _appRslt              :: Rslt
-  , _shownInResultsWindow :: ShownInResultsWindow
-  }
+type ViewQuery = String
+
+data ViewResult = ViewResult {
+    _viewResultAddr :: Addr
+  , _viewResultString :: String } deriving (Show, Eq, Ord)
+
+data ViewMembers = ViewMembers { _mvCenter :: Addr }
+  deriving (Show, Eq, Ord)
+
+data View = VQuery      ViewQuery
+          | VResult     ViewResult
+          | VMembers    ViewMembers
+          | VCenterRole ViewCenterRole deriving (Eq, Ord)
+
+-- | `ViewCenterRole` is used to group relationships in which the `Expr`at
+-- `crvCenter` appears. For instance, if the `Expr` at `Addr 3` helps some things,
+-- then `ViewCenterRole 3 (RoleMember 1) ["", "helps", ""]` will
+-- be one of the groups of relationships involving the `Expr` at `Addr 3`.
+data ViewCenterRole = ViewCenterRole {
+    _crvCenter :: Addr
+  , _crvRole   :: Role
+  , _crvTplt   :: [Expr] } deriving (Eq, Ord)
+
+makePrisms      ''View -- prisms!
+makeLenses      ''ViewResult
+makeLenses      ''ViewCenterRole
+
+instance Show ViewCenterRole where
+  show vcr = let
+    tplt = vcr ^. crvTplt
+    noLeft     = error "show ViewCenterRole: impossible"
+    noRslt     = error "show ViewCenterRole: Rslt irrelevant"
+    noMiscount = error "show ViewCenterRole: This math is good."
+    showTplt = either (const noLeft) id
+               $ eShow noRslt (Tplt tplt)
+    in if vcr ^. crvRole == RoleTplt
+       then "Tplt " ++ showTplt
+       else let (ar :: Arity) = length tplt - 1
+                RoleMember (n :: Int) = vcr ^. crvRole
+                mbrs = either (const noMiscount) id
+                       $ replaceNth (Phrase $ "it") n
+                       $ replicate ar $ Phrase "_"
+            in either (const noLeft) id
+               $ eShow noRslt $ Rel mbrs $ Tplt tplt
+
+instance Show View where
+  show (VQuery x)      = "VQuery "      ++ show x
+  show (VResult x)     = "VResult "     ++ show x
+  show (VMembers x)    = "VMembers "    ++ show x
+  show (VCenterRole x) = "VCenterRole " ++ show x
+
+
+-- | = Huge types.
+
+-- PITFALL: These types must come last to deive `Show`.
 
 data ViewTree = ViewTree {
     _viewChildFocus :: Int -- ^ meaningless if `viewSubviews` empty
@@ -60,38 +109,20 @@ data ViewTree = ViewTree {
   -- or `VCenterRole`s. A `VQuery` can be nowhere but the top of the tree.
   } deriving (Show)
 
-data View = VQuery      ViewQuery
-          | VResult     ViewResult
-          | VMembers    ViewMembers
-          | VCenterRole ViewCenterRole deriving (Show, Eq, Ord)
+data St = St {
+    _focusRing            :: B.FocusRing WindowName
+  , _viewTree             :: ViewTree
+  , _pathToFocus          :: Path
+  , _uiError              :: String
+  , _commands             :: B.Editor String WindowName
+  , _appRslt              :: Rslt
+  , _shownInResultsWindow :: ShownInResultsWindow
+  }
 
-type ViewQuery = String
-
-data ViewResult = ViewResult {
-    _viewResultAddr :: Addr
-  , _viewResultString :: String } deriving (Show, Eq, Ord)
-
-data ViewMembers = ViewMembers { _mvCenter :: Addr }
-  deriving (Show, Eq, Ord)
-
--- | `ViewCenterRole` is used to group relationships in which the `Expr`at
--- `crvCenter` appears. For instance, if the `Expr` at `Addr 3` helps some things,
--- then `ViewCenterRole 3 (RoleMember 1) ["", "helps", ""]` will
--- be one of the groups of relationships involving the `Expr` at `Addr 3`.
-data ViewCenterRole = ViewCenterRole {
-    _crvCenter :: Addr
-  , _crvRole :: Role
-  , _crvTplt :: [Expr] } deriving (Show, Eq, Ord)
-
-makeLenses ''St
-makePrisms ''View -- prisms!
-makeLenses ''ViewTree
-makeLenses ''ViewResult
-makeLenses ''ViewCenterRole
-
+makeLenses      ''St
+makeLenses      ''ViewTree
 makeBaseFunctor ''ViewTree
-makeLenses ''ViewTreeF
-
+makeLenses      ''ViewTreeF
 
 instance Show St where
   show st = "St { "
