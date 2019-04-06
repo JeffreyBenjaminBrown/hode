8063f904777a0a4c9f6d4f40d163a2946211c4c3
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-04-06 06:44:43 -0500

    make the format more terse

--- src/Rslt/Files.hs
+++ src/Rslt/Files.hs
@@ -3,6 +3,7 @@
 module Rslt.Files where
 
 import qualified Data.Map       as M
+import           Text.Regex
 import           System.Directory (listDirectory)
 import           System.FilePath.Posix (dropExtension, takeExtension)
 
@@ -21,8 +22,10 @@ readRslt p0 = do
   files <- filter (\f -> takeExtension f == ".rslt")
            <$> listDirectory p0
   (es :: [(Addr, RefExpr)]) <- let
-      f p = do (e :: RefExpr) <- read <$> readFile (p0 ++ "/" ++ p)
-               let (a :: Addr) = read $ dropExtension p
+      f p = do (e :: RefExpr) <-
+                 read . _unBrief <$> readFile (p0 ++ "/" ++ p)
+               let (a :: Addr) =
+                     read $ dropExtension p
                return (a,e)
       in mapM f files
   return $ mkRslt $ M.fromList es
@@ -31,6 +34,23 @@ writeRslt :: FilePath -> Rslt -> IO ()
 writeRslt p r = let
   writeRefExpr :: (Addr, RefExpr) -> IO ()
   writeRefExpr (a,e) =
-    writeFile name $ show e ++ "\n"
+    writeFile name $ _brief $ show e ++ "\n"
     where name = p ++ "/" ++ show a ++ ".rslt"
   in mapM_ writeRefExpr $ M.toList $ _addrToRefExpr r
+
+_brief :: String -> String
+_brief = subRegex_safe "^Phrase' "     "p " .
+         subRegex_safe "^Tplt' "       "t " .
+         subRegex_safe "^Rel' \\(Rel " "r "
+
+_unBrief :: String -> String
+_unBrief = subRegex_safe "^p " "Phrase' "   .
+           subRegex_safe "^t " "Tplt' "     .
+           subRegex_safe "^r " "Rel' (Rel "
+
+subRegex_safe :: String -> String -> String -> String
+subRegex_safe pat replacement input =
+  let regex = mkRegex pat
+  in case matchRegex regex input of
+    Nothing -> input
+    Just _  -> subRegex regex input replacement
