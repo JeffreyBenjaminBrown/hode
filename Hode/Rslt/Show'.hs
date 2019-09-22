985cf0153b3c9bb95bd9aaba549f89628fa55b09
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-22 00:25:52 -0500

    update for new Tplt type

--- Hode/Rslt/Show'.hs
+++ Hode/Rslt/Show'.hs
@@ -2,19 +2,20 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ViewPatterns #-}
 
-module Hode.Rslt.Show' (
-    eParenShow' -- ^ Int -> Rslt -> Expr -> Either String AttrString
+module Hode.Rslt.Show'
+  ( eParenShow' -- ^ Int -> Rslt -> Expr -> Either String AttrString
   , hashUnlessEmptyStartOrEnd' -- ^ Int -> [AttrString] -> [AttrString]
   ) where
 
 import           Data.Functor.Foldable
+import           Data.Maybe
 import qualified Data.List as L
 
 import Hode.Brick
 import Hode.Rslt.RLookup
 import Hode.Rslt.RTypes
 import Hode.Rslt.RUtil
-import Hode.Rslt.Show
+import Hode.Rslt.Show.Wut
 import Hode.Util.Misc
 import Hode.Util.UParse
 
@@ -41,39 +42,25 @@ eParenShow' maxDepth r e0 =
   g (_, AddrF _) = Left "impossible; given earlier unAddrRec."
   g (_, PhraseF p) = Right [(p,textColor)]
 
-  g (_, ExprTpltF js0) = do
-    -- prefixLeft "g of Tplt: " $ do
-    js1 :: AttrString <-
-      concat <$> ifLefts (map f js0)
-    Right $ L.intersperse (" _ ", textColor) js1
+  g (_, ExprTpltF t) = do
+    Tplt ma bs mc :: Tplt AttrString <- ifLefts $ fmap f t
+    Right $ concat $ L.intersperse space
+      ( maybeToList ma ++
+        zip' (repeat blank) bs ++
+        maybeToList mc )
 
-  g (n, ExprRelF (Rel ms0 (Fix (EFW (_, ExprTpltF js0))))) =
+  g (n, ExprRelF (Rel ms0 (Fix (EFW (_, ExprTpltF t))))) =
     prefixLeft "g of Rel: " $ do
     ms1 :: [AttrString] <- ifLefts $ map f ms0
-    js1 :: [AttrString] <- hashUnlessEmptyStartOrEnd' n <$>
-                           ifLefts (map f js0)
-
-    let content :: [(AttrString,AttrString)] =
-          zip ( [("",textColor)] : ms1 ) js1
-        space :: AttrString = [(" ", textColor)]
-        null' :: AttrString -> Bool
-        null' = (==0). sum . map (length . fst)
-        concat' :: [AttrString] -> AttrString
-        concat' = foldl k [] where
-          k :: AttrString -> AttrString -> AttrString
-          k (null' -> True) (null' -> True) = []
-          k (null' -> True) s = s
-          k s (null' -> True) = s
-          k s t = s ++ t
-
-    Right $ attrStrip
-      $ concatMap (\(m,j) -> concat' [ attrStrip m, space
-                                     , attrStrip j, space ] )
-      $ content
+    Tplt ma bs mc :: Tplt AttrString <-
+      ifLefts $ fmap (hash n) <$> fmap f t
+    Right $ concat $ L.intersperse space $
+      maybeToList ma ++
+      zip' ms1 bs ++
+      maybeToList mc
 
   g (_, ExprRelF (Rel _ _)) = Left $
     "g given a Rel with a non-Tplt in the Tplt position."
-
 
 -- | `hashUnlessEmptyStartOrEnd k js` adds `k` #-marks to every joint
 -- in `js`, unless it's first or last and the empty string.
@@ -92,9 +79,6 @@ hashUnlessEmptyStartOrEnd' k0 joints = case joints' of
       then [("(",sepColor)] ++ s ++ [(")",sepColor)]
       else s
 
-  hash :: Int -> AttrString -> AttrString
-  hash k s = (replicate k '#', sepColor) : s
-
   hashUnlessEmpty :: Int -> AttrString -> AttrString
   hashUnlessEmpty _ [("",_)] = [("",textColor)]
   hashUnlessEmpty k s = hash k s
@@ -104,3 +88,12 @@ hashUnlessEmptyStartOrEnd' k0 joints = case joints' of
   hashUnlessEmptyEnd k [s]      =  [hashUnlessEmpty k s]
   hashUnlessEmptyEnd k (s : ss) =   hash               k s
                                   : hashUnlessEmptyEnd k ss
+
+blank :: AttrString
+blank = [("_", textColor)]
+
+space :: AttrString
+space = [(" ", textColor)]
+
+hash :: Int -> AttrString -> AttrString
+hash k s = (replicate k '#', sepColor) : s
