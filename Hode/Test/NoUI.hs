a7daa0fdcecd0b0c64784c24b7d153d19f4c186b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-09-23 04:28:12 -0500

    toward testing Hode.Rslt.Edit.delete

--- Hode/Test/NoUI.hs
+++ Hode/Test/NoUI.hs
@@ -13,7 +13,7 @@ import Hode.Rslt.RTypes
 
 test_module_NoUI :: Test
 test_module_NoUI = TestList [
-  TestLabel "test_insert" test_insert
+  TestLabel "test_insert" test_insert,
   ]
 
 test_insert :: Test
@@ -33,3 +33,23 @@ test_insert = TestCase $ do
                   ( S.fromList $ M.elems $ _addrToRefExpr s2) )
   assertBool "" ( Right r2 ==
                   nInserts (mkRslt mempty) [ab,cd] )
+
+-- -- It's hard to write a natural test of deletion,
+-- -- because deleting a phrase at address X causes
+-- -- (Phrase "") to move to that location.
+-- Right abRel_cdRel = nInserts (mkRslt mempty)
+--                     [ "a # b", "c # d" ]
+-- Right abRel_cd =    nInserts (mkRslt mempty)
+--                     [ "a # b", "c", "d" ]
+-- > _addrToRefExpr <$> Right abRel_cdRel
+-- (fromList [(0,Phrase' ""),(1,Tplt' (Tplt Nothing [0] Nothing)),
+--            (2,Phrase' "a"),(3,Phrase' "b"),(4,Rel' (Rel [2,3] 1)),
+--            (5,Phrase' "c"),(6,Phrase' "d"),(7,Rel' (Rel [5,6] 1))])
+-- > _addrToRefExpr <$> nDelete abRel_cdRel "c # d"
+-- (fromList [               (1,Tplt' (Tplt Nothing [7] Nothing)),
+--             (2,Phrase' "a"),(3,Phrase' "b"),(4,Rel' (Rel [2,3] 1)),
+--             (5,Phrase' "c"),(6,Phrase' "d"),(7,Phrase' "")])
+-- > _addrToRefExpr <$> Right abRel_cd
+-- (fromList [(0,Phrase' ""),(1,Tplt' (Tplt Nothing [0] Nothing)),
+--            (2,Phrase' "a"),(3,Phrase' "b"),(4,Rel' (Rel [2,3] 1)),
+--            (5,Phrase' "c"),(6,Phrase' "d")])
