18c601c3af2038ca27a86c1a2f313c79fcca1cef
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-12-23 20:39:14 -0500

    untested   -- | If the focused buffer has cycles recorded in ,   -- this recomputes them.   updateFocusedBufferCycles :: St -> Either String St

--- Hode/UI/CycleBreaker.hs
+++ Hode/UI/CycleBreaker.hs
@@ -4,9 +4,12 @@
 module Hode.UI.CycleBreaker where
 
 import           Control.Lens
+import qualified Data.Set as S
 import qualified Data.List.PointedList as P
 
+import Hode.Hash.HLookup
 import Hode.PTree.Initial
+import Hode.Rslt.Binary
 import Hode.Rslt.RTypes
 import Hode.UI.BufferRowTree
 import Hode.UI.IUtil
@@ -16,6 +19,28 @@ import Hode.UI.Types.Views
 import Hode.UI.Window
 import Hode.Util.Misc
 
+
+-- | If the focused buffer has cycles recorded in `bufferCycles`,
+-- this recomputes them.
+updateFocusedBufferCycles :: St -> Either String St
+-- TODO ? This is inefficient -- it recomputes all cycles each time.
+updateFocusedBufferCycles st = do
+  cs0 :: [Cycle] <-
+    case st ^? stGet_focusedBuffer . _Just . bufferCycles
+    of Nothing -> Left "Focused buffer not found."
+       Just cs -> Right cs
+
+  if null cs0 then Right st
+    else do
+    let uniqueKernels :: [(TpltAddr,Addr)] =
+          S.toList . S.fromList -- discard duplicates
+          $ map (_2 %~ head) cs0
+        ci (tplt, start) = cyclesInvolving
+          (st ^. appRslt) SearchLeftward tplt start
+    cs1 :: [Cycle] <-
+      concat <$> mapM ci uniqueKernels
+    Right $ st &
+      stSet_focusedBuffer . bufferCycles .~ cs1
 
 -- | Updates the cycle buffer to reflect what is now in
 -- the focused buffer's `bufferCycles` field.
