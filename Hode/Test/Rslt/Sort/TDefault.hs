11b37f171e41bf420c4215dc08fc066f5bb527f6
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-01-02 01:49:19 -0500

    add a passing test

--- Hode/Test/Rslt/Sort/TDefault.hs
+++ Hode/Test/Rslt/Sort/TDefault.hs
@@ -4,6 +4,7 @@
 
 module Hode.Test.Rslt.Sort.TDefault where
 
+import           Data.Either
 import qualified Data.Set as S
 import           Test.HUnit
 
@@ -16,6 +17,7 @@ import Hode.Rslt.Sort.Default
 
 test_module_rslt_sort_default :: Test
 test_module_rslt_sort_default = TestList [
+  TestLabel "test_usesTplt" test_usesTplt,
   TestLabel "test_isIn_usingTplt" test_isIn_usingTplt,
   TestLabel "test_sortTpltsForSorting" test_sortTpltsForSorting
   ]
@@ -77,3 +79,21 @@ test_isIn_usingTplt = TestCase $ do
   assertBool "bc is in no a-rel" $
     isIn_usingTplt r (tplt "c") (expr "bc")
     == Right True
+
+test_usesTplt :: Test
+test_usesTplt = TestCase $ do
+  let Right (r :: Rslt) = nInserts (mkRslt mempty)
+        [ "a #a"
+        , "b #b"     -- unused except to guarantee a search doesn't fail
+        , "a #b c"
+        , "b #c a" ] -- unused except to guarantee a search doesn't fail
+      get s = fst . head $ ( either (error "not present") id
+                             $ nFind r s )
+  assertBool "(a #a) uses the tplt (_ a)" $ True ==
+    usesTplt r (get "/t /_ a") (get "a #a")
+  assertBool "(a #a) does not use the tplt (_ b)" $ False ==
+    usesTplt r (get "/t /_ b") (get "a #a")
+  assertBool "(a #b c) uses the tplt (_ b _)" $ True ==
+    usesTplt r (get "/t /_ b /_") (get "a #b c")
+  assertBool "(a #b c) does not use the tplt (_ c _)" $ False ==
+    usesTplt r (get "/t /_ c /_") (get "a #b c")
