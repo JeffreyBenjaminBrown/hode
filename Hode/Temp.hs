7c3e9ee278a8eb36a10bd44bfc5d76f51f19a575
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-03-28 18:47:32 -0500

    delete kruft

--- Hode/Temp.hs
+++ Hode/Temp.hs
@@ -3,40 +3,9 @@
 
 module Hode.Temp where
 
-import qualified Data.List.PointedList as P
-
-import           Control.Lens
-import qualified Data.Map as M
+--import qualified Data.List.PointedList as P
+--import           Control.Lens
+--import qualified Data.Map as M
 --import           Data.Set (Set)
 --import qualified Data.Set as S
---
---import Hode.Test.Rslt.TSort
-import Hode.Hode
-
-
-showColumns :: ExprRow -> [ColorString]
-showColumns er = let
-  (inSort, selected) = _sortAndSelectColumnProps er
-  in [ ( if selected then "" else "x"
-       , if inSort then TextColor else SepColor ) ] :
-     ( map ((:[]) . (, TextColor) . show)
-         . M.elems . _numColumnProps
-         $ er )
-
-showNode :: ExprRow -> ColorString
-showNode = showColor defaulViewOptions . _viewExprNode
-
-getFolded :: ExprRow -> Bool
-getFolded = _folded . _otherProps
-
-padCols :: Porest ExprRow -> Porest (ExprRow, [[(String, Color)]])
-padCols = porestWithPaddedColumns colorStringLength toColorString showColumns 
-
-
--- * Stale? The problem happens upstream of showPorest, in porestWithPaddedColumns
-
-sp :: St -> [(Bool, ColorString, ColorString)]
-sp st = let
-  Just (x :: PTree ExprRow) = st ^? stGetFocused_ViewExprNode_Tree . _Just
-  Just (xt :: Porest ExprRow) = P.fromList [x]
-  in showPorest colorStringLength toColorString showColumns showNode getFolded xt
+--import Hode.Hode
