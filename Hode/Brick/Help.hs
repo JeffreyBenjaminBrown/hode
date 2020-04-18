a0bf5f5d98d531b7b7165ba0ebed8645df64a90e
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-04-18 16:29:49 -0500

    tab between windows

--- Hode/Brick/Help.hs
+++ Hode/Brick/Help.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings,
+TemplateHaskell #-}
 
 import Control.Lens
 import           Data.Foldable (toList)
@@ -16,10 +17,25 @@ import qualified Brick.Util           as B
 import qualified Brick.Widgets.Core   as B
 
 
-type Name = ()
 type Focused = Bool
-type St = P.PointedList (String, BaseMenu)
 type BaseMenu = P.PointedList (String, String)
+
+-- | Choice 1 determines what is available inn Choice 2,
+-- and Choice 2 determines what is available in Choice 3.
+-- Collectively, they determine what is in the content window.
+data WindowName = Choice1 | Choice2 | Choice3 | Content
+  deriving (Show, Eq, Ord)
+makeLenses ''WindowName
+allWindowNammes = maybe (error "impossible") id $
+                  P.fromList [ Choice1, Choice2, Choice3, Content ]
+
+data St = St { _choices :: P.PointedList (String, BaseMenu)
+             , _windows :: P.PointedList WindowName }
+  deriving (Show, Eq)
+makeLenses ''St
+
+isFocusedWindow :: St -> WindowName -> Bool
+isFocusedWindow st = (==) (st ^. windows . P.focus)
 
 animals :: P.PointedList (String, String)
 animals =
@@ -33,9 +49,9 @@ balls :: P.PointedList (String, String)
 balls =
   maybe (error "impopssible") id $
   P.fromList [ ("Basketball","Very bouncy.")
+             , ( "Mercury","Bigger than a rugby ball, smaller than Saturn." )
              , ( "Softball","Lies! What the hell?" )
-             , ( "Tennis ball", "Somehow extra awesome." )
-             , ( "Mercury","Usually not in the form of a ball." ) ]
+             , ( "Tennis ball", "Somehow extra awesome." ) ]
 
 chemicals :: P.PointedList (String, String)
 chemicals =
@@ -45,11 +61,13 @@ chemicals =
              , ( "capsaicin", "Intense. Probably misspelled." )
              , ( "DNA", "Hardest language ever." ) ]
 
-menus :: St
-menus = maybe (error "impossible") id $
-  P.fromList [ ("animals",   animals)
-             , ("balls",     balls)
-             , ("chemicals", chemicals) ]
+initState :: St
+initState = St
+  { _windows = allWindowNammes
+  , _choices = maybe (error "impossible") id $
+               P.fromList [ ("animals",   animals)
+                          , ("balls",     balls)
+                          , ("chemicals", chemicals) ] }
 
 pListToList_withFocus :: (a -> b) -> (a -> b) -> P.PointedList a -> [b]
 pListToList_withFocus normal focus as =
@@ -59,39 +77,59 @@ pListToList_withFocus normal focus as =
 
 ui :: St -> B.Widget n
 ui st = let
-  (c :: String, b :: BaseMenu) = st ^. P.focus
-  normal    = B.padLeftRight 1 . B.withAttr "option"                  . B.str
-  highlight = B.padLeftRight 1 . B.withAttr ("option" <> "highlight") . B.str
+  cs :: P.PointedList (String, BaseMenu) = st ^. choices
+  (c :: String, b :: BaseMenu) = cs ^. P.focus
+  normal    stylePrefix = B.padLeftRight 1 . B.withAttr style . B.str
+    where style = stylePrefix
+  highlight stylePrefix = B.padLeftRight 1 . B.withAttr style . B.str
+    where style = stylePrefix <> "highlight"
   in
-  B.hBox [ B.vBox $ pListToList_withFocus normal highlight $ fmap fst b
-         , B.vBorder
-         , normal $ b^. P.focus . _2 ]
-  B.<=> ( B.vLimit 1 $ B.hBox $
-          L.intersperse B.vBorder $
-          pListToList_withFocus normal highlight $ fmap fst st )
+  B.hBox ( let sp = case st ^. windows . P.focus of
+                      Choice3 -> "focus"
+                      _       -> "no focus"
+           in [ B.vBox $ pListToList_withFocus
+                (normal sp) (highlight sp) $ fmap fst b
+              , B.vBorder
+              , normal sp $ b ^. P.focus . _2 ] )
+  B.<=> ( let sp = case st ^. windows . P.focus of
+                      Choice2 -> "focus"
+                      _       -> "no focus"
+          in B.vLimit 1 $ B.hBox $ L.intersperse B.vBorder $
+             pListToList_withFocus (normal sp) (highlight sp) $
+             fmap fst cs )
 
 theMap :: B.AttrMap
 theMap = B.attrMap
          (V.white `B.on` V.black) -- default
-         [ ("option",                  V.white `B.on` V.black)
-         , ("option" <> "highlight", V.white `B.on` V.blue) ]
+         [ ("no focus",                V.white `B.on` V.black)
+         , ("no focus" <> "highlight", V.white `B.on` V.blue) 
+         , ("focus",                   V.black `B.on` V.green)
+         , ("focus" <> "highlight",    V.white `B.on` V.blue) ]
 
-respond :: St -> B.BrickEvent Name e -> B.EventM Name (B.Next St)
+respond :: St -> B.BrickEvent WindowName e
+              -> B.EventM WindowName (B.Next St)
 respond st (B.VtyEvent ev) =
-    case ev of
-        V.EvKey V.KEsc [] -> B.halt st
+  case ev of
+    V.EvKey V.KEsc [] -> B.halt st
+    V.EvKey (V.KChar ' ') [] ->
+      B.continue $ st & windows %~ C.next
+    _ -> case st ^. windows . P.focus of
+      Choice3 -> case ev of
         V.EvKey (V.KChar 'e') [] ->
-          B.continue $ st & P.focus . _2 %~ C.previous
+          B.continue $ st & choices . P.focus . _2 %~ C.previous
         V.EvKey (V.KChar 'd') [] ->
-          B.continue $ st & P.focus . _2 %~ C.next
-        V.EvKey (V.KChar 's') [] ->
-          B.continue $ st & C.previous
-        V.EvKey (V.KChar 'f') [] ->
-          B.continue $ st & C.next
+          B.continue $ st & choices . P.focus . _2 %~ C.next
         _ -> B.continue st
+      Choice2 -> case ev of
+        V.EvKey (V.KChar 'e') [] ->
+          B.continue $ st & choices %~ C.previous
+        V.EvKey (V.KChar 'd') [] ->
+          B.continue $ st & choices %~ C.next
+        _ -> B.continue st
+      _ -> B.continue st
 respond st _ = B.continue st
 
-app :: B.App St e ()
+app :: B.App St e WindowName
 app = B.App
       { B.appDraw = (:[]) . ui
       , B.appHandleEvent = respond
@@ -101,4 +139,4 @@ app = B.App
       }
 
 main :: IO St
-main = B.defaultMain app menus
+main = B.defaultMain app initState
